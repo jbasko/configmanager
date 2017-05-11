3be54c82189a05eb3223bf3707375e911ab89ef5
Jazeps Basko <jazeps.basko@gmail.com>
2017-05-11 08:05:04 +0100

    Bump version

--- configmanager/__init__.py
+++ configmanager/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.15'
+__version__ = '0.0.16'
 
 from .base import not_set, ConfigItem, ConfigManager
 from .exceptions import UnknownConfigItem, ConfigValueNotSet, UnsupportedOperation
